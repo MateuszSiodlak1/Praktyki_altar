5a139f3452b85745d14ddfff45b49d86e25ba674
MateuszSiodlak1 <mateuszx2008@gmail.com>
2026-08-17 11:35:54 +0200

    konfiguracja modelu OpenCLIP z wielojęzycznym enkoderem

--- 22_porównanie_wyszukiwań/embedding_databases_creator.py
+++ 22_porównanie_wyszukiwań/embedding_databases_creator.py
@@ -6,3 +6,12 @@ import numpy as np
 from PIL import Image
 from qdrant_client import QdrantClient
 from qdrant_client.models import VectorParams, Distance, PointStruct
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model_name = "xlm-roberta-base-ViT-B-32"
+pretrained = "laion5b_s13b_b90k"
+
+model, _, preprocess = open_clip.create_model_and_transforms(model_name, pretrained=pretrained)
+model = model.to(device)
+model.eval()
+tokenizer = open_clip.get_tokenizer(model_name)
